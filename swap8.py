db3fe0bd0085074b133c282031954d691ca510d3
Navya K <navikk99@gmail.com>
2023-03-18 14:56:37 +0530

    Add files via upload

---
+++ swap8.py
@@ -0,0 +1,19 @@
+#swap 2 variables without using third variable
+
+a=5
+b=11
+
+a=a+b#16
+b=a-b#5-16=11
+a=a-b#16-11=5
+
+print('a:', a)
+print('b:', b)
+
+#another methods:
+
+#  a,b=b,a   works only in python
+
+#a=a^b
+#b=a^b
+#a=a^b
